d49a13c92c18b1100da01e28b07624cd6d84b648
ajidexx <aaron-jarvis@idexx.com>
2017-09-20 17:49:47 -0400

    .

--- campaigns/admin.py
+++ campaigns/admin.py
@@ -5,8 +5,11 @@ class MailCampaignAdmin(admin.ModelAdmin):
     def start_campaign_link(self):
         return "<a href='/campaigns/mailcampaign/%s/start'>start campaign</a>" % self.id
 
+    def vendor_contact_count(self):
+        return self.vendor_contacts.count()
+
     start_campaign_link.allow_tags = True
-    list_display = ('name', 'job', 'message_template', 'is_active', start_campaign_link)
+    list_display = ('name', 'job', 'message_template', vendor_contact_count, 'is_active', start_campaign_link)
 
 class MessageTemplateAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active')
