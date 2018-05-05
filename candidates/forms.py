5744effc67eacb5a8915f7b033afff17253a82f6
ajidexx <aaron-jarvis@idexx.com>
2018-05-04 20:22:20 -0400

    public site

--- candidates/forms.py
+++ candidates/forms.py
@@ -1,21 +1,28 @@
 from django import forms
 from django_countries.fields import LazyTypedChoiceField
 from django_countries import countries
-from recruiting.choices import (TIMEZONE_CHOICES, EDUCATION_CHOICES,
-	 GENDER_CHOICES)
+from recruiting.choices import (TIMEZONE_CHOICES, EDUCATION_CHOICES, GENDER_CHOICES)
 
 class UserApplyStep1Form(forms.Form):
-	first_name = forms.CharField(max_length=20)
-	last_name = forms.CharField(max_length=20)
-	email = forms.EmailField(max_length=50)
-	citizenship = LazyTypedChoiceField(choices=countries)
-	skype_id = forms.CharField(label='Skype ID', max_length=20)
-	timezone = forms.ChoiceField(choices=TIMEZONE_CHOICES)
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
+    email = forms.EmailField(max_length=50)
+    citizenship = LazyTypedChoiceField(choices=countries)
+    skype_id = forms.CharField(label='Skype ID', max_length=20)
+    timezone = forms.ChoiceField(choices=TIMEZONE_CHOICES)
 
 class UserApplyStep2Form(forms.Form):
-	birth_year = forms.IntegerField(min_value=1950, max_value=2000)
-	gender = forms.TypedChoiceField(choices=GENDER_CHOICES)
-	education = forms.TypedChoiceField(choices=EDUCATION_CHOICES)
-	education_major = forms.CharField(max_length=50)
-	image = forms.ImageField()
-	resume = forms.FileField()
+    birth_year = forms.IntegerField(min_value=1950, max_value=2000)
+    gender = forms.TypedChoiceField(choices=GENDER_CHOICES)
+    education = forms.TypedChoiceField(choices=EDUCATION_CHOICES)
+    education_major = forms.CharField(max_length=50)
+    image = forms.ImageField()
+    resume = forms.FileField()
+
+class ApplyNowForm(forms.Form):
+    firstName = forms.CharField(max_length=30, required=True)
+    lastName = forms.CharField(max_length=100, required=True)
+    email = forms.CharField(max_length=100, required=True)
+    phone = forms.CharField(max_length=20, required=True)
+    message = forms.CharField(max_length=1000, required=False)
+    resume = forms.FileField(required=False)
--- jobs/views.py
+++ jobs/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from models import Job
-from candidates.models import Candidate
+from candidates.models import Candidate, CandidateDocument
 from interviews.models import InterviewRequest
 from accounts.models import UserProfile
 from django_mailbox.models import Mailbox
@@ -12,61 +12,87 @@ from django.template.loader import get_template
 from django.template import Context
 import datetime as dt
 from datetime import date, datetime
+from candidates.forms import ApplyNowForm
+from django.contrib.auth.models import User
+
 
 """ PUBLIC SITE VIEWS """
+
+
 def home(request):
     context = {}
     return render(request, 'jobs/home.html', context)
+
 
 def services(request):
     context = {}
     return render(request, 'jobs/services.html', context)
 
+
 def portfolio(request):
     context = {}
     return render(request, 'jobs/portfolio.html', context)
+
 
 def about(request):
     context = {}
     return render(request, 'jobs/about.html', context)
 
+
 def careers(request):
-    key = request.GET.get('key', None)
-    user = UserProfile.verify_token(key)
-
-    if request.method == 'GET':
-        jobs = Job.objects.filter(is_active__exact=True).filter(submission_date__lte=date.today()).order_by('-created')
-        context = {'jobs': jobs}
-
-    if request.method == 'POST':
-        jobs_ids = request.POST.getlist('requested_jobs[]')
-
-        if request.user.is_anonymous() and (not key or not user):
-            request.session['add_new_jobs_pending'] = True
-            request.session['requested_jobs'] = jobs_ids
-            request.session['redirect_to'] = reverse('jobs')
-            return HttpResponseRedirect(reverse('account_login'))
-
-        context = {}
-
-        if not user:
-            user = request.user
-
-        add_interview_requests(request, user, jobs_ids)
-
+    jobs = Job.objects.filter(is_active__exact=True).filter(submission_date__gte=date.today()).order_by('-created')
+    context = {'jobs': jobs}
     return render(request, 'jobs/careers.html', context)
+
 
 def career_details(request, job_id):
     job = Job.objects.get(id=job_id)
     context = {'job': job}
     return render(request, 'jobs/career_details.html', context)
 
+
 def career_apply(request, job_id):
     job = Job.objects.get(id=job_id)
-    context = {'job': job}
-    return render(request, 'jobs/career_apply.html', context)
+
+    # make sure this is a post
+    if request.method == 'POST':
+        # create the new document
+        form = ApplyNowForm(request.POST, request.FILES)
+        if form.is_valid():
+            try:
+                user = User.objects.create_user(form.cleaned_data['firstName'], 'job%s-%s' % (job.id, form.cleaned_data['email']), 'changeme')
+                user.first_name = form.cleaned_data['firstName']
+                user.last_name = form.cleaned_data['lastName']
+                user.save()
+
+                userProfile = UserProfile()
+                userProfile.user = user
+                userProfile.phone_number = form.cleaned_data['phone']
+                userProfile.save()
+
+                candidate = Candidate()
+                candidate.user = user
+                candidate.save()
+
+                interviewRequest = InterviewRequest()
+                interviewRequest.candidate = candidate
+                interviewRequest.job = job
+                interviewRequest.save()
+
+                doc = CandidateDocument(document=request.FILES['resume'])
+                doc.display_name = form.cleaned_data['resume']
+                doc.candidate = candidate
+                doc.save()
+            except:
+                return render(request, 'jobs/career_apply.html', {'job': job, 'status': 'error'})
+
+        return render(request, 'jobs/career_apply.html', {'job': job, 'status': 'success'})
+    else:
+        return render(request, 'jobs/career_apply.html', {'job': job, 'status': 'new'})
+
 
 """ SECURE SITE VIEWS """
+
 
 def add_interview_requests(request, user, jobs_ids):
     try:
@@ -138,7 +164,7 @@ def fetch(request):
 
 # XML job feed.  Used by indeed.
 def xml_feed(request):
-    jobs = Job.objects.filter(is_active__exact=True)
+    jobs = Job.objects.filter(is_active__exact=True).filter(is_featured__exact=True).filter(submission_date__gte=date.today()).order_by('-id')
     context = {'jobs': jobs}
 
     template = get_template('jobs/xml_feed.html')
