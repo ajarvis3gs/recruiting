d486d05d17b0dc5b46cdd94cacc4ef0b4df62580
ajidexx <aaron-jarvis@idexx.com>
2018-05-08 14:05:10 -0400

    public site

--- recruiting/settings.py
+++ recruiting/settings.py
@@ -146,7 +146,7 @@ EMAIL_SUBJECT_PREFIX = ""
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATICFILES_DIRS = (BASE_DIR, 'static')
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
