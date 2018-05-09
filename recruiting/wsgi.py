a14702f7b4f200211190c4af11188edfce18e09d
ajidexx <aaron-jarvis@idexx.com>
2018-05-09 08:22:55 -0400

    public site

--- recruiting/wsgi.py
+++ recruiting/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recruit.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recruiting.settings")
 
 application = get_wsgi_application()
