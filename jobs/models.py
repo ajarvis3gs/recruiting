0e934e7880273f994ba51c008d64837d6f3d23ac
ajidexx <aaron-jarvis@idexx.com>
2018-08-03 16:33:28 -0400

    .

--- jobs/models.py
+++ jobs/models.py
@@ -17,8 +17,8 @@ class Country(models.Model):
 class Job(models.Model):
     employer = models.ForeignKey(Employer, on_delete=models.CASCADE, related_name='jobs')
     title = models.CharField(max_length=100)
-    employer_contact = models.ForeignKey(EmployerContact, related_name='jobs', null=True)
-    pricing_schedule = models.ForeignKey(EmployerPricingSchedule, related_name='jobs', null=True)
+    employer_contact = models.ForeignKey(EmployerContact, related_name='jobs', null=True, blank=True)
+    pricing_schedule = models.ForeignKey(EmployerPricingSchedule, related_name='jobs', null=True, blank=True)
     agency = models.CharField(blank=True, null=True, max_length=100)
     location = models.CharField(blank=True, null=True, max_length=100)
     target_rate = models.DecimalField(blank=True, null=True, max_digits=5, decimal_places=2)
@@ -36,10 +36,16 @@ class Job(models.Model):
     is_active = models.BooleanField(default=True)
     last_modified = models.DateTimeField(auto_now_add=False, auto_now=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='jobs', null=True)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='jobs', null=True, blank=True)
 
     def __str__(self):
         return "%s: %s" % (self.id, self.title)
+
+    def getJobTitle(self):
+        if self.pricing_schedule:
+            return self.pricing_schedule.job_title
+        else:
+            return self.title
 
 class JobMandatoryQualification(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='mandatoryQualifications')
